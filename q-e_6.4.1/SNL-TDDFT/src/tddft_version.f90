e71c7e0f5bf0230a4c7c45fba109ec3c00550917
adbacze <adbacze@sandia.gov>
2019-08-21 15:41:44 -0600

    Add git version stuff from the code of Davide.

---
+++ q-e_6.4.1/SNL-TDDFT/src/tddft_version.f90
@@ -0,0 +1,20 @@
+!
+! Copyright (C) 2003-2013 Quantum ESPRESSO group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+
+! NOTE: this file is automatically created
+
+!----------------------------------------------------------------------------
+MODULE tddft_version
+  !
+  IMPLICIT NONE
+  !
+  SAVE
+  !
+  CHARACTER (LEN=40) :: tddft_git_revision = "08e3b0a5236ce8d125913f4d2234a77c1105277d"
+  !
+END MODULE tddft_version
