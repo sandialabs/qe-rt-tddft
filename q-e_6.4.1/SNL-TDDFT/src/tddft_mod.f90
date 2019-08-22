04ef4f62b0c5b1c9c2c13dc0f1ec0e8094026a87
adbacze <adbacze@sandia.gov>
2019-08-22 09:12:31 -0600

    Starting to object orient.

---
+++ q-e_6.4.1/SNL-TDDFT/src/tddft_mod.f90
@@ -0,0 +1,14 @@
+MODULE tddft_mod
+  !-----------------------------------------------------------------------
+  !
+  ! ... This module contains the variables used for TDDFT calculations
+  !
+  USE kinds,               ONLY : dp
+  USE fixed_occ,           ONLY : tfixed_occ, f_inp ! occupations from input
+  USE dynamics_module,     ONLY : dt                ! timestep 
+  IMPLICIT NONE
+  SAVE
+
+  ! flow control flags
+  
+END MODULE tddft_mod
